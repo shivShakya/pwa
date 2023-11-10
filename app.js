b92021fc975652ecaa33424cd150b11035554fc0
Shivam Shakya <shivdu2000@gmail.com>
2023-11-10 19:51:37 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const container = document.querySelector(".container")
+const coffees = [
+  { name: "Perspiciatis", image: "images/coffee1.jpeg" },
+  { name: "Voluptatem", image: "images/coffee2.jpeg" },
+  { name: "Explicabo", image: "images/coffee3.jpeg" },
+  { name: "Rchitecto", image: "images/coffee4.jpeg" },
+  { name: " Beatae", image: "images/coffee5.jpeg" },
+  { name: " Vitae", image: "images/coffee6.jpeg" }
+]
+
+
+const showCoffees = () => {
+    let output = ""
+    coffees.forEach(
+      ({ name, image }) =>
+        (output += `
+                <div class="card">
+                  <img class="card--avatar" src=${image} />
+                  <h1 class="card--title">${name}</h1>
+                  <a class="card--link" href="#">Taste</a>
+                </div>
+                `)
+    )
+    container.innerHTML = output
+  }
+  document.addEventListener("DOMContentLoaded", showCoffees)
+
+
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function() {
+      navigator.serviceWorker
+        .register("/serviceWorker.js")
+        .then(res => console.log("service worker registered"))
+        .catch(err => console.log("service worker not registered", err))
+    })
+  }
